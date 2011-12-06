73ce5b3f9118b62b2326dd10c360fa209db0d213
Peter Macko <pmacko@eecs.harvard.edu>
2011-12-05 19:09:52 -0500

    Implemented a parser for the server responses in the RDF/SPARQL protocol.

--- backends/cpl-rdf/cpl-connection.h
+++ backends/cpl-rdf/cpl-connection.h
@@ -37,9 +37,11 @@
 
 #include <private/cpl-platform.h>
 #include <cpl.h>
-#include <string>
 
 #include <curl/curl.h>
+#include <map>
+#include <string>
+#include <vector>
 
 
 
@@ -75,6 +77,13 @@ typedef struct {
 } cpl_rdf_connection_t;
 
 
+/**
+ * The result set
+ */
+class RDFResultSet;
+
+
+
 /***************************************************************************/
 /** Functions                                                             **/
 /***************************************************************************/
@@ -96,6 +105,138 @@ cpl_rdf_connection_init(const char* url);
  */
 void
 cpl_rdf_connection_close(cpl_rdf_connection_t* connection);
+
+/**
+ * Execute a query
+ *
+ * @param connection the connection handle
+ * @param statement the query statement
+ * @param out the pointer to an already initialized result set
+ * @return an error code
+ */
+cpl_return_t
+cpl_rdf_connection_execute_query(cpl_rdf_connection_t* connection,
+								 const char* statement,
+								 RDFResultSet* out);
+
+/**
+ * Execute an update statement
+ *
+ * @param connection the connection handle
+ * @param statement the update statement
+ * @param out the pointer to an already initialized result set (to store
+ *            error messages, can be NULL)
+ * @return an error code
+ */
+cpl_return_t
+cpl_rdf_connection_execute_update(cpl_rdf_connection_t* connection,
+								  const char* statement,
+								  RDFResultSet* out = NULL);
+
+
+
+/***************************************************************************/
+/** Result Set                                                            **/
+/***************************************************************************/
+
+/**
+ * A value type
+ */
+typedef enum {
+	RDF_T_STRING,
+	RDF_T_URI,
+} RDFValueType;
+
+/**
+ * A single value
+ */
+typedef struct {
+	std::string raw;
+	RDFValueType type;
+	union {
+		const char* v_string;
+		const char* v_uri;
+	};
+} RDFValue;
+
+/**
+ * A single result in the result set
+ */
+typedef std::map<std::string, RDFValue*> RDFResult;
+
+/**
+ * The result set
+ */
+class RDFResultSet
+{
+
+public:
+
+	/**
+	 * Create an empty result set
+	 */
+	RDFResultSet(void);
+
+	/**
+	 * Destroy the result set
+	 */
+	~RDFResultSet(void);
+
+	/**
+	 * Determine the number of results
+	 *
+	 * @return the number of results
+	 */
+	inline size_t
+	size(void) { return m_results.size(); }
+
+	/**
+	 * Return the given result from the result set
+	 *
+	 * @param index the index between 0 and size()-1
+	 * @return a reference to the result
+	 */
+	inline RDFResult&
+	operator[] (size_t index) { return *(m_results[index]); }
+
+	/**
+	 * Return the vector of error messages
+	 *
+	 * @return a reference to the vector of error messages
+	 */
+	inline const std::vector<std::string>&
+	error_messages(void) { return m_errors; }
+
+	/**
+	 * Append a result to the result set
+	 *
+	 * @param result the result (will be destroyed together with this object)
+	 */
+	inline void
+	append(RDFResult* result) { m_results.push_back(result); }
+
+	/**
+	 * Append an error message to the vector of error messages
+	 *
+	 * @param format the format of the exception
+	 * @param ... the arguments of the format
+	 */
+	void
+	append_error_message(const char* format, ...);
+
+
+protected:
+
+	/**
+	 * The results
+	 */
+	std::vector<RDFResult*> m_results;
+
+	/**
+	 * The error messages
+	 */
+	std::vector<std::string> m_errors;
+};
 
 
 #endif
