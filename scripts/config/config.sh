28002a73bd91cc67802097801cc46a371b50f759
jackson okuhn <jpokuhn@jacksons-MacBook-Pro.local>
2018-01-29 16:14:47 -0500

    script debugged

--- scripts/config/config.sh
+++ scripts/config/config.sh
@@ -19,22 +19,22 @@ if ! command -v $ODBCINST >/dev/null 2>&1; then
 	exit 1
 fi
 
-if ! find /usr -name psqlodbcw.so | egrep '.*'; then
+if ! find /usr -name psqlodbcw.so | egrep '.*' >/dev/null 2>&1; then
 	echo "psqlodbcw.so not found. Please install psqlodbc and try again."
 	exit 1
 fi
 
-if ! find /usr -name boost | egrep '.*'; then
+if ! find /usr -name boost | egrep '.*' >/dev/null 2>&1; then
 	echo "Boost libraries not found. Please install and try again."
 	exit 1
 fi
 
-if ! find /usr -name nlohmann_json | egrep '.*'; then
+if ! find /usr -name nlohmann_json | egrep '.*' >/dev/null 2>&1; then
 	echo "nlohmann_json not found. Please install and try again."
 	exit 1
 fi
 
-if ! find /usr -name swig | egrep '.*'; then
+if ! find /usr -name swig | egrep '.*' >/dev/null 2>&1; then
 	echo "swig not found. Please install SWIG and try again."
 	exit 1
 fi
@@ -45,54 +45,74 @@ fi
 
 echo "We'll need some information before we get started."
 
-read -p "Postgres ADMIN username (default 'postgres'): " POSTGRES_ADMIN
-POSTGRES_ADMIN=${POSTGRES_ADMIN:-postgres}
-read -p -s "Postgres ADMIN password (default ''): " POSTGRES_ADMIN_PASSWORD
-POSTGRES_ADMIN_PASSWORD=${POSTGRES_ADMIN_PASSWORD:-}
-read -p "Postgres server address (default 'localhost'): " POSTGRES_SERVER
-POSTGRES_SERVER=${POSTGRES_SERVER:-localhost}
-read -p "Postgres server port (default '5432'): " POSTGRES_PORT
-POSTGRES_PORT=${POSTGRES_PORT:-5432}
+read -p "Postgres ADMIN username (default 'postgres'): " POSTGRES_ADMIN;
+POSTGRES_ADMIN=${POSTGRES_ADMIN:-postgres};
+echo "username: $POSTGRES_ADMIN"
 
+read -s -p "Postgres ADMIN password (default ''): " POSTGRES_ADMIN_PASSWORD;
+POSTGRES_ADMIN_PASSWORD=${POSTGRES_ADMIN_PASSWORD:-};
+echo
 
-LOCAL_CONNECTION=0
-if POSTGRES_SERVER -eq 'localhost' || POSTGRES_SERVER -eq '127.0.0.1'; then
-	$LOCAL_CONNECTION = 1
+read -p "Postgres server address (default 'localhost'): " POSTGRES_SERVER;
+POSTGRES_SERVER=${POSTGRES_SERVER:-localhost};
+echo "address: $POSTGRES_SERVER"
+
+read -p "Postgres server port (default '5432'): " POSTGRES_PORT;
+POSTGRES_PORT=${POSTGRES_PORT:-5432};
+echo "port: $POSTGRES_PORT"
+
+LOCAL_CONNECTION=0;
+if [ "$POSTGRES_SERVER" == "localhost" ] || [ "$POSTGRES_SERVER" == "127.0.0.1" ]; then
+	LOCAL_CONNECTION=1
 fi
 
 # check if connection is possible
-export PGPASSWORD = $POSTGRES_ADMIN_PASSWORD
-CHECK_CONNECTIONS
-if ! $($PSQL -U $POSTGRES_ADMIN -h POSTGRES_SERVER -p POSTGRES_PORT &>/dev/null); then
-	if [$LOCAL_CONNECTION]; then
-		echo "We haven't been able to connect to the local instance of PostgresQL as the admin user.\n"
+export PGPASSWORD=$POSTGRES_ADMIN_PASSWORD
+$PSQL -U $POSTGRES_ADMIN -h $POSTGRES_SERVER -p $POSTGRES_PORT -c 'SELECT version()' >/dev/null 2>&1
+if ! [ $? ]; then
+	if [ $LOCAL_CONNECTION ]; then
+		echo "We haven't been able to connect to the local instance of PostgresQL as the admin user."
 		echo "Is postgresql running?"
-		echo "Also, please make sure that the daemon is listening to network connections!"
-		echo "   - at least on the localhost interface."
+		echo "Also, please make sure that the daemon is listening to network connections"
+		echo "on the localhost interface."
 		echo "Finally, did you supply the correct admin password?"
 		echo "   Don't know the admin password for your Postgres installation?"
-		echo "   - then simply set the access level to \"trust\" temporarily (for localhost only!)"
+		echo "   Just set the access level to \"trust\" temporarily (for localhost only!)"
 		echo "   in your pg_hba.conf file."
 		exit 1
 	else 
-		echo "We haven't been able to connect to the remote Postgres server as the admin user."
-		echo "(Or you simply don't have psql installed on this server)"
+		echo "We haven't been able to connect to the remote Postgres server as the admin user"
+		echo "(or you simply don't have psql installed on this server)."
 		echo "Also, please make sure the Postgres server is accepting connections from this IP address!"
 		echo "   - this may involve editing your pg_hba.conf file on the remote machine."
 		exit 1
 	fi
+fi
 
 echo "Your credentials seem to be in order. Moving on to configuring the CPL database."
 
-read -p "Postgres CPL database name: " POSTGRES_DATABASE
-read -p "Postgres CPL username: " POSTGRES_USER
-read -p "Postgres CPL user password: " POSTGRES_USER_PASSWORD
+read -p "Postgres CPL database name (default 'cpl'): " POSTGRES_DATABASE
+POSTGRES_DATABASE=${POSTGRES_DATABASE:-cpl};
+echo "database name: $POSTGRES_DATABASE"
+read -p "Postgres CPL username (default 'cpl'): " POSTGRES_USER
+POSTGRES_USER=${POSTGRES_USER:-cpl};
+echo "database user: $POSTGRES_USER"
+read -s -p "Postgres CPL user password (default 'cplcplcpl'): " POSTGRES_USER_PASSWORD
+POSTGRES_USER_PASSWORD=${POSTGRES_USER_PASSWORD:-cplcplcpl};
+echo
 
 # configure Postgres database
-$PSQL -U $POSTGRES_ADMIN -h POSTGRES_SERVER -p POSTGRES_PORT \
+$PSQL -U $POSTGRES_ADMIN -h $POSTGRES_SERVER -p $POSTGRES_PORT \
 	-v db_name=$POSTGRES_DATABASE \
 	-v user_name=$POSTGRES_USER \
-	-v user_password=$POSTGRES_USER_PASSWORD < ../postgresql-setup-conf.sql
+	-v user_password=$POSTGRES_USER_PASSWORD < ../postgresql-setup-conf.sql >/dev/null 2>&1
+
+if ! [ $? ]; then
+  echo "Something went wrong with database configuration."
+  echo "Make sure your database and user names are valid strings."
+  exit 1
+fi
+
 
 echo "Great. Moving on to configuring ODBC settings."
 
@@ -105,13 +125,17 @@ Driver = psqlodbcw.so \n" > CPL_odbcinst.txt
 
 $ODBCINST -i -d -f CPL_odbcinst.txt
 
+rm CPL_odbcinst.txt
+
 # Verify 
-if ! $($ODBCINST -q -d | grep -q '[PostgreSQL Unicode]') ; then
+$ODBCINST -q -d | grep -q '[PostgreSQL Unicode]'
+if ! [ $? ]; then
   echo "Postgres ODBC driver configuration failed."
   echo "Check that you've downloaded the dependency"
   echo "from https://odbc.postgresql.org/."
   echo "You can always manually modify odbcinst.ini"
   echo "or use a ODBC administrator tool."
+  exit 1
 fi
 
 # Configure CPL ODBC connection
@@ -129,11 +153,15 @@ Password        = $POSTGRES_USER_PASSWORD \n" > CPL_odbc.txt
 
 $ODBCINST -i -s -f CPL_odbc.txt
 
+rm CPL_odbc.txt
+
 # Verify 
-if ! $($ODBCINST -q -s | grep -q '[CPL]'); then
+$ODBCINST -q -s | grep -q '[CPL]'
+if ! [ $? ]; then
   echo "CPL ODBC configuration failed."
   echo "You can always manually modify the ~/.odbc.ini file"
   echo "or use a ODBC administrator tool."
+  exit 1
 fi
 
 echo "Configuration successful."
