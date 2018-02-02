16a1dc105f92059e11b489aa66f47df1a592f6f3
jackson okuhn <jpokuhn@jacksons-MacBook-Pro.local>
2018-02-02 12:29:50 -0500

    added correct status codes

--- bindings/python/RestAPI/cpl-rest.py
+++ bindings/python/RestAPI/cpl-rest.py
@@ -1,5 +1,5 @@
 import CPL
-from CPL import cpl_bundle, cpl_object, cpl_relation
+from CPL import cpl_bundle, cpl_object, cpl_relation, CPLException
 from flask import Flask, request, jsonify
 
 connection = CPL.cpl_connection()
@@ -40,270 +40,220 @@ def serialize_property_tuple(prop):
         'value': prop[2]
     }
 
+
+@app.errorhandler(CPLException)
+def handle_cpl_error(e):
+    if e.code == CPL.E_INVALID_ARGUMENT or e.code == CPL.E_ALREADY_EXISTS:
+        response = jsonify(error=e, success=False)
+        response.status_code = 400
+        return response
+    if e.code == CPL.E_NOT_FOUND:
+        response = jsonify(error=e, success=False)
+        response.status_code = 400
+        return response
+    else:
+        response = jsonify(error=e, success=False)
+        response.status_code = 500
+        return response
+
+@app.errorhandler(LookupError)
+def handle_lookup_error():
+    response = jsonify(error="Lookup failed", success=False)
+    response.status_code = 404
+    return response
+
+@app.errorhandler(Exception)
+def handle_exception():
+    response = jsonify(error="Unknown internal error", success=False)
+    response.status_code = 500
+    return response
+
 # TODO: wrap returns in make_response with http status codes
 @app.route("/provapi/version")
 def version_get():
-    try:
-        return jsonify(version=CPL.CPL_VERSION_STR)
-    except Exception as e:
-        return jsonify(error=str(e))
+    return jsonify(version=CPL.CPL_VERSION_STR)
 
 @app.route("/provapi/bundle/<int:id>")
 def bundle_get(id):
-    try:
-        info = cpl_bundle(id).info()
-        return jsonify(name=info.name,
-                       creation_time=info.creation_time,
-                       creation_session=info.creation_session)
-    except Exception as e:
-        return jsonify(error=str(e))
+    info = cpl_bundle(id).info()
+    return jsonify(name=info.name,
+                   creation_time=info.creation_time,
+                   creation_session=info.creation_session)
 
 @app.route("/provapi/bundle", methods=['POST'])
 def bundle_post():
-    try:
-        content = request.get_json()
-        bundle = connection.create_bundle(content['name'])
-        return jsonify(id=bundle.id)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    bundle = connection.create_bundle(content['name'])
+    return jsonify(id=bundle.id)
 
 @app.route("/provapi/lookup/bundle", methods=['POST'])
 def bundle_lookup():
-    try:
-        content = request.get_json()
-        bundle = connection.lookup_bundle(content['name'])
-        return jsonify(id=bundle.id)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    bundle = connection.lookup_bundle(content['name'])
+    return jsonify(id=bundle.id)
 
 @app.route("/provapi/lookup/bundles", methods=['POST'])
 def bundles_lookup():
-    try:
-        content = request.get_json()
-        bundles = connection.lookup_all_bundles(content['name'])
-        return jsonify(ids=[bundle.id for bundle in bundles])
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    bundles = connection.lookup_all_bundles(content['name'])
+    return jsonify(ids=[bundle.id for bundle in bundles])
 
 @app.route("/provapi/bundle/<int:id>/prefix")
 @app.route("/provapi/bundle/<int:id>/prefix/<string:prefix>")
 def bundle_prefix_get(id, prefix=None):
-    try:
-        prefixes = cpl_bundle(id).prefixes(prefix)
-        return jsonify(prefixes=[serialize_prefix_tuple(pre) for pre in prefixes])
-    except Exception as e:
-        return jsonify(error=str(e))
+    prefixes = cpl_bundle(id).prefixes(prefix)
+    return jsonify(prefixes=[serialize_prefix_tuple(pre) for pre in prefixes])
 
 @app.route("/provapi/bundle/<int:id>/prefix", methods=['POST'])
 def bundle_prefix_post(id):
-    try:
-        content = request.get_json()
-        cpl_bundle(id).add_prefix(content['prefix'],
-                                  content['iri'])
-        return jsonify(success=True)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    cpl_bundle(id).add_prefix(content['prefix'],
+                              content['iri'])
+    return jsonify(success=True)
 
 @app.route("/provapi/bundle/<int:id>/property")
 @app.route("/provapi/bundle/<int:id>/property/<string:prefix>:<string:name>")
 def bundle_property_get(id, prefix=None, name=None):
-    try:
-        properties = cpl_bundle(id).properties(prefix, name)
-        return jsonify(properties=[serialize_property_tuple(prop) for prop in properties])
-    except Exception as e:
-        return jsonify(error=str(e))
+    properties = cpl_bundle(id).properties(prefix, name)
+    return jsonify(properties=[serialize_property_tuple(prop) for prop in properties])
 
 @app.route("/provapi/bundle/<int:id>/property", methods=['POST'])
 def bundle_property_post(id):
-    try:
-        content = request.get_json()
-        cpl_bundle(id).add_property(content['prefix'],
-                                    content['name'],
-                                    content['value'])
-        return jsonify(success=True)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    cpl_bundle(id).add_property(content['prefix'],
+                                content['name'],
+                                content['value'])
+    return jsonify(success=True)
 
 @app.route("/provapi/bundle/<int:id>/objects")
 def bundle_objects_get(id):
-    try:
-        objects = connection.get_bundle_objects(cpl_bundle(id))
-        return jsonify(objects=[serialize_object_info(o) for o in objects])
-    except Exception as e:
-        return jsonify(error=str(e))
+    objects = connection.get_bundle_objects(cpl_bundle(id))
+    return jsonify(objects=[serialize_object_info(o) for o in objects])
 
 @app.route("/provapi/bundle/<int:id>/relations")
 def bundle_relations_get(id):
-    try:
-        relations = connection.get_bundle_relations(cpl_bundle(id))
-        return jsonify(relations=[serialize_relation_info(r) for r in relations])
-    except Exception as e:
-        return jsonify(error=str(e))
+    relations = connection.get_bundle_relations(cpl_bundle(id))
+    return jsonify(relations=[serialize_relation_info(r) for r in relations])
 
 @app.route("/provapi/object/<int:id>")
 def object_get(id):
-    try:
-        info = cpl_object(id).info
-        return jsonify(serialize_object_info(info))
-    except Exception as e:
-        return jsonify(error=str(e))
+    info = cpl_object(id).info
+    return jsonify(serialize_object_info(info))
 
 @app.route("/provapi/object", methods=['POST'])
 def object_post():
-    try:
-        content = request.get_json()
-        o = connection.create_object(content['prefix'],
-                                     content['name'],
-                                     content['type'],
-                                     cpl_bundle(content['bundle']))
-        return jsonify(id=o.id)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    o = connection.create_object(content['prefix'],
+                                 content['name'],
+                                 content['type'],
+                                 cpl_bundle(content['bundle']))
+    return jsonify(id=o.id)
 
 @app.route("/provapi/lookup/object", methods=['POST'])
 def object_lookup():
-    try:
-        content = request.get_json()
-        o = connection.lookup_object(content['prefix'],
-                                     content['name'],
-                                     content['type'],
-                                     cpl_bundle(content['bundle']
-                                                if content['bundle'] 
-                                                else None))
-        return jsonify(id=o.id)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    o = connection.lookup_object(content['prefix'],
+                                 content['name'],
+                                 content['type'],
+                                 cpl_bundle(content['bundle']
+                                            if content['bundle'] 
+                                            else None))
+    return jsonify(id=o.id)
 
 @app.route("/provapi/lookup/objects", methods=['POST'])
 def objects_lookup():
-    try:
-        content = request.get_json()
-        objects = connection.lookup_all_objects(content['prefix'],
-                                                content['name'],
-                                                content['type'],
-                                                cpl_bundle(content['bundle']
-                                                           if content['bundle'] 
-                                                           else None))
-        return jsonify(ids=[o.id for o in objects])
-    except Exception as e:
-        return jsonify(error=str(e))    
+    content = request.get_json()
+    objects = connection.lookup_all_objects(content['prefix'],
+                                            content['name'],
+                                            content['type'],
+                                            cpl_bundle(content['bundle']
+                                                       if content['bundle'] 
+                                                       else None))
+    return jsonify(ids=[o.id for o in objects])    
 
 @app.route("/provapi/object/<int:id>/property")
 @app.route("/provapi/object/<int:id>/property/<string:prefix>:<string:property>")
 def object_property_get(id, prefix=None, property=None):
-    try:
-        properties = cpl_object(id).properties(prefix, property)
-        return jsonify(properties=
-            [serialize_property_tuple(prop) for prop in properties])
-    except Exception as e:
-        return jsonify(error=str(e))
+    properties = cpl_object(id).properties(prefix, property)
+    return jsonify(properties=
+        [serialize_property_tuple(prop) for prop in properties])
 
 
 @app.route("/provapi/object/<int:id>/property")
 def object_property_post(id):
-    try:
-        content = request.get_json()
-        cpl_object(id).add_property(content['prefix'],
-                                    content['name'],
-                                    content['value'])
-        return jsonify(success=True)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    cpl_object(id).add_property(content['prefix'],
+                                content['name'],
+                                content['value'])
+    return jsonify(success=True)
 
 @app.route("/provapi/lookup/object/property", methods=['POST'])
 def object_lookup_by_property():
-    try:
-        content = request.get_json()
-        objects = connection.lookup_by_property(content['prefix'],
-                                                content['key'],
-                                                content['value'])
-        return jsonify(ids=[o.id for o in objects])
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    objects = connection.lookup_by_property(content['prefix'],
+                                            content['key'],
+                                            content['value'])
+    return jsonify(ids=[o.id for o in objects])
 
 @app.route("/provapi/object/<int:id>/relation/ancestors")
 def object_ancestors_get(id):
-    try:
-        relations = cpl_object(id).relations()
-        return jsonify(relations=[serialize_relation_info(info) for info in relations])
-    except Exception as e:
-        return jsonify(error=str(e))
+    relations = cpl_object(id).relations()
+    return jsonify(relations=[serialize_relation_info(info) for info in relations])
 
 @app.route("/provapi/object/<int:id>/descendants")
 def object_descendants_get(id):
-    try:
-        relations = cpl_object(id).relations(CPL.D_DESCENDANTS)
-        return jsonify([serialize_relation_info(info) for info in relations])
-    except Exception as e:
-        return jsonify(error=str(e))
+    relations = cpl_object(id).relations(CPL.D_DESCENDANTS)
+    return jsonify([serialize_relation_info(info) for info in relations])
 
 @app.route("/provapi/object/<int:id>/relation", methods=['POST'])
 def object_relation_post(id):
-    try:
-        content = request.get_json()
-        if content['dest']:
-            relation = cpl_object(id).relation_to(cpl_object(content['dest']),
-                                                  content['type'],
-                                                  cpl_bundle(content['bundle']))
-        else:
-            relation = cpl_object(id).relation_from(cpl_object(content['src']),
-                                                    content['type'],
-                                                    cpl_bundle(content['bundle']))
-        return jsonify(id=relation.id)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    if content['dest']:
+        relation = cpl_object(id).relation_to(cpl_object(content['dest']),
+                                              content['type'],
+                                              cpl_bundle(content['bundle']))
+    else:
+        relation = cpl_object(id).relation_from(cpl_object(content['src']),
+                                                content['type'],
+                                                cpl_bundle(content['bundle']))
+    return jsonify(id=relation.id)
 
 @app.route("/provapi/relation/<int:id>/property")
 @app.route("/provapi/relation/<int:id>/property/<string:prefix>:<string:name>")
 def relation_property_get(id, prefix=None, name=None):
-    try:
-        properties = cpl_relation(id, None, None, None, None, None).properties(prefix, name)
-        return jsonify(properties)
-    except Exception as e:
-        return jsonify(error=str(e))
+    properties = cpl_relation(id, None, None, None, None, None).properties(prefix, name)
+    return jsonify(properties)
 
 @app.route("/provapi/relation/<int:id>/property", methods=['POST'])
 def relation_property_post(id):
-    try:
-        content = request.get_json()
-        cpl_relation(id, None, None, None, None, None).add_property(content['prefix'],
-                                                                    content['name'],
-                                                                    content['value'])
-        return jsonify(success=True)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    cpl_relation(id, None, None, None, None, None).add_property(content['prefix'],
+                                                                content['name'],
+                                                                content['value'])
+    return jsonify(success=True)
 
 @app.route("/provapi/json/validate", methods=['POST'])
 def json_validate():
-    try:
-        content = request.get_json()
-        ret = connection.validate_json(content['JSON'])
-        if ret == None:
-            return jsonify(success=True)
-        else:
-            return jsonify(error=ret.out_string)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    ret = connection.validate_json(content['JSON'])
+    if ret == None:
+        return jsonify(success=True)
+    else:
+        return jsonify(error=ret.out_string)
 
 @app.route("/provapi/json", methods=['POST'])
 def json_post():
-    try:
-        content = request.get_json()
-        anchors = [(cpl_object(a['id']), a['name']) for a in content['anchor_objects']]
-        bundle = connection.import_document_json(content['JSON'],
-                                        content['bundle_name'],
-                                        anchors)
-        return jsonify(id=bundle.id)
-    except Exception as e:
-        return jsonify(error=str(e))
+    content = request.get_json()
+    anchors = [(cpl_object(a['id']), a['name']) for a in content['anchor_objects']]
+    bundle = connection.import_document_json(content['JSON'],
+                                    content['bundle_name'],
+                                    anchors)
+    return jsonify(id=bundle.id)
 
 @app.route("/provapi/bundle/<int:id>/json")
 def bundle_json_get(id):
-    try:
-        json = connection.export_bundle_json([cpl_bundle(id)])
-        return jsonify(JSON=json)
-    except Exception as e:
-        return jsonify(error=str(e))
+    json = connection.export_bundle_json([cpl_bundle(id)])
+    return jsonify(JSON=json)
 
 
 if __name__ == "__main__":
